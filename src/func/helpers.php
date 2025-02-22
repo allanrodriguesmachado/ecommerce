b17d354a004ca13dbdce87b83fec4e0ecf3ee596
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2025-02-21 23:27:57 -0300

    Alter func bashPath

--- src/func/helpers.php
+++ src/func/helpers.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 if (!function_exists('bashPath')) {
     function bashPath(string $path): string
     {
-        return dirname(__DIR__) . '/' . $path;
+        return dirname(__DIR__, 2) . '/' . $path;
     }
 }
 
 if(!function_exists('mix')) {
     function mix(string $path)
     {
-//        if (!file_exists(bashPath('/../../public/mix-manifest.json'))) {
-//            throw new RuntimeException('mix-manifest.json not found');
-//        }
+        if (!file_exists(bashPath('public/mix-manifest.json'))) {
+            throw new RuntimeException('mix-manifest.json not found');
+        }
 
         $content = file_get_contents(__DIR__ . '/../../public/mix-manifest.json');
         $content = json_decode($content, true);
