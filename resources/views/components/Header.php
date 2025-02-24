a53a4d21139ce3782780d799c8c8e64bd0a041d9
Allan Rodrigues - Developer <consultoria01@shibata.corp.br>
2025-02-24 16:21:03 -0300

    Alter

--- resources/views/components/Header.php
+++ resources/views/components/Header.php
@@ -3,7 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href='<?= mix('/css/app.css') ?>'>
-
     <title><?= $title ?> </title>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </head>
 <body>
